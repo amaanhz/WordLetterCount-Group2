fcd848c43ade588ad26a1d27c82f085fb570dec1
amaanhz <amaanhamza15@gmail.com>
2025-02-17 15:03:29 +0000

    Got Spark working

---
+++ src/main/java/org/wordlettercount/SimpleApp.java
@@ -0,0 +1,19 @@
+package org.wordlettercount;
+
+import org.apache.spark.sql.SparkSession;
+import org.apache.spark.sql.Dataset;
+
+public class SimpleApp {
+    public static void main(String[] args) {
+        String logFile = "/opt/spark/README.md"; // Should be some file on your system
+        SparkSession spark = SparkSession.builder().appName("SimpleApp").config("spark.master", "local").getOrCreate();
+        Dataset<String> logData = spark.read().textFile(logFile).cache();
+
+        long numAs = logData.filter((org.apache.spark.api.java.function.FilterFunction<String>)s -> s.contains("a")).count();
+        long numBs = logData.filter((org.apache.spark.api.java.function.FilterFunction<String>)s -> s.contains("b")).count();
+
+        System.out.println("Lines with a: " + numAs + ", lines with b: " + numBs);
+
+        spark.stop();
+    }
+}
