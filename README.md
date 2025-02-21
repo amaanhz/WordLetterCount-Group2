50d423356fafec75a705456afedf74f5fe5cdf96
Arvind Raghu <arvind.r03@gmail.com>
2025-02-21 11:00:00 +0000

    run-experiments empty file, and SimpleApp initial notes
    Added:
    - Script run-experiments.py with detailed todos
    - README.md more details
    - SimpleApp.java simple function for word count
    - SimpleApp.java all todos necessary
    
    To do:
    - all todos present in files
    - plus focus on optimisation of java file

--- README.md
+++ README.md
@@ -10,6 +10,18 @@ After fetching and unzipping the zipped folder, the only command to run is:
 
 This script will activate the Python virtual environment, download any module dependencies e.g. `seaborn` for plotting etc., and then run `run-experiments.sh` and `plot-experiments.py` in order, feeding the timestamp output of the former into the latter.
 
-## NOTE:
+## NOTES:
+
+### Scripting
 
 The scripting was designed for `Python v3.12`, which is the version of Python present on the remote server.
+
+### Batch vs. Sequential
+
+Since the aim of the project is accurate timing measurements of execution, varying the number of executors and input file size, to minimize resource contention and performance degradation as a result of this (which would then affect the accuracy and validity of the experiment measurements), sequential execution is favoured.
+
+This is executed as suggested in the deliverables: starting a new experiment every hour i.e. requiring 27 hours total to run all experiments.
+
+### TODOs not mentioned in code:
+
+- Remove `test-timestamp` dir and related contents from `experiments` when finished with scripting
--- src/main/java/org/wordlettercount/SimpleApp.java
+++ src/main/java/org/wordlettercount/SimpleApp.java
@@ -2,14 +2,28 @@ package org.wordlettercount;
 
 import org.apache.spark.sql.SparkSession;
 import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.functions;
+import org.apache.spark.sql.Encoders;
+import java.util.Arrays;
 
 /**
  *  Class to run distributed Spark job to count words.
  * 
- *  Weights word frequency as specified in deliverable. 
+ *  Weights word frequency as specified in deliverable.
+ * 
+ *  TODO: modify so that the code takes in argument of input file
+ *  i.e. call like ./example.jar -i ./input/file/path.txt
+ * 
+ *  TODO: modify so that the code writes to mounted volume, specifically
+ *  to ./test-data/CloudComputingCoursework_Group2/
+ *  which should contain both words_spark.csv and letters_spark.csv
  */
 public class SimpleApp {
     public static void main(String[] args) {
+        // Check arguments are present and valid
+        // TODO: implement check that -i flag is present, and path to file exists and is valid type
+
         String logFile = "/opt/spark/README.md"; // Should be some file on your system
         SparkSession spark = SparkSession.builder().appName("SimpleApp").config("spark.master", "local").getOrCreate();
         Dataset<String> logData = spark.read().textFile(logFile).cache();
@@ -20,5 +34,45 @@ public class SimpleApp {
         System.out.println("Lines with a: " + numAs + ", lines with b: " + numBs);
 
         spark.stop();
+    }
+
+    // TODO: add equivalent function to below for letter counting
+
+    /**
+     *  Implementation of word count.
+     * 
+     *  Should pass in session, and also stop session after (i.e. this is not handled by the function).
+     */
+    public void wordCountImplementation(
+        SparkSession sparkSession,
+        String inputFilePath, 
+        String outputFilePath
+    ) {
+        Dataset<String> text = sparkSession
+            .read()
+            .textFile(inputFilePath)
+            .cache();
+
+        // TODO: change to identify words using space and given punctuation marks
+        // TODO: specifically omit words that include non-alphabetic characters
+        // TODO: change to do it by lowercase
+        // TODO: change to omit words that do not fit into three given categories
+        // TODO: change to store words by first category (popular, common then rare) then by char ascending order i.e. a to z
+        // TODO: change to have rank, word/letter, category and frequency columns
+        Dataset<Row> wordCounts = text
+            .flatMap(
+                (String lineOfWords) -> Arrays.asList(lineOfWords.split(" ").iterator()),
+                Encoders.STRING()
+            )                  // TODO: flatmap here requires function that abides by the above todos
+            .groupBy("value")
+            .count()
+            .orderBy(functions.desc("count"));
+
+        wordCounts
+            .coalesce(1)                // TODO: find more efficient way - this is expensive for large data
+            .write()
+            .option("header", "true")
+            .option("delimiter", ",")   // NOTE: important that the delimiter does not have spaces
+            .csv(outputFilePath);       // TODO: make sure we are writing to csv in the correct format i.e. look in `test-timestamp` dir in `experiments` for example, pay attention to spaces
     }
 }
